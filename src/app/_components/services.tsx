1c32cafbb238946cce7d3717dc6ed26e5cca14d5
Micael <micaelxv1@gmail.com>
2025-02-27 15:08:13 -0300

    services

--- src/app/_components/services.tsx
+++ src/app/_components/services.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { WhatsappLogo } from "@phosphor-icons/react";
 import useEmblaCarousel from "embla-carousel-react";
-import { ChevronLeft, ChevronRight, Scissors, Syringe, CarTaxiFront, Hotel } from "lucide-react";
+import { ChevronLeft, ChevronRight, Scissors, Syringe, CarTaxiFront, Hotel, Clock } from "lucide-react";
 
 const services = [
   {
@@ -41,7 +41,7 @@ const services = [
 export function Services() {
 
   const [emblaRef, emblaApi] = useEmblaCarousel({ 
-    loop: true,
+    loop: false,
     align: 'start',
     slidesToScroll: 1,
     breakpoints: {
@@ -51,6 +51,14 @@ export function Services() {
     }
   });
 
+  function scrollPrev() {
+    emblaApi?.scrollPrev();
+  }
+
+  function scrollNext() {
+    emblaApi?.scrollNext();
+  }
+  
   return (
     <section className="bg-white py-16">
       <div className="container mx-auto px-4">
@@ -64,13 +72,60 @@ export function Services() {
               {services.map((item, index) => (
                 // O flex-[0_0_100%] quer dizer que o item vai ocupar 100% do espaço disponível
                 <div key={index} className="flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3">
-                  <article className="bg-[#1e293b] text-white rounded-2xl space-y-4 h-full flex flex-col">
+                  <article className="bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col">
+                    {/* o flex-1 é para ele pegar a tela toda */}
+                    <div className="flex-1 flex items-start justify-between">  
+                      
+                      <div className="flex gap-3">
+                        <span className="text-3xl">{item.icon}</span>
+                        <div>
+                          <h3 className="font-bold text-xl my-1">{item.title}</h3>
+                          <p className="text-gray-400 text-sm select-none">
+                            {item.description}
+                          </p>
+                        </div>
+                      </div>
+
+                    </div>
+
+                    <div className="border-t border-gray-700 pt-4 flex items-center justify-between">
+                      <div className="flex items-center gap-2 text-sm">
+                        <Clock className="w-5 h-5"/>
+                        <span>{item.duration}</span>
+                      </div>
+
+                      <a 
+                        href="#"
+                        className="flex items-center justify-center gap-2 hover:bg-red-500 px-4 py-1 rounded-md duration-300"
+                      >
+                        <WhatsappLogo className="w-5 h-5"/>
+                        Entrar em contato
+                      </a>
+                    </div>
 
                   </article>
                 </div>
               ))}
             </div>
           </div>
+          
+          <button 
+            onClick={scrollPrev} 
+            className="bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10"
+          >
+            <ChevronLeft 
+              className="w-6 h-6 text-gray-600"
+            />
+          </button>
+
+          <button 
+            onClick={scrollNext} 
+            className="bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-6 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10"
+          >
+            <ChevronRight 
+              className="w-6 h-6 text-gray-600"
+            />
+          </button>
 
         </div>
       </div>
