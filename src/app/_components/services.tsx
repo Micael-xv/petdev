6af16781c167a1a2f8adbaa7b7726c3cf92f678e
Micael <micaelxv1@gmail.com>
2025-04-16 22:28:46 -0300

    fix: ajustes

--- src/app/_components/services.tsx
+++ src/app/_components/services.tsx
@@ -1,54 +1,68 @@
 "use client";
 import { WhatsappLogo } from "@phosphor-icons/react";
 import useEmblaCarousel from "embla-carousel-react";
-import { ChevronLeft, ChevronRight, Scissors, Syringe, CarTaxiFront, Hotel, Clock } from "lucide-react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  Scissors,
+  Syringe,
+  CarTaxiFront,
+  Hotel,
+  Clock,
+} from "lucide-react";
 
 const services = [
   {
     title: "Banho & Tosa",
-    description: "Inclui banho com produtos específicos para o tipo de pelagem e pele do animal, corte de unhas, limpeza das orelhas e tosa personalizada (higiênica ou estilizada).",
+    description:
+      "Inclui banho com produtos específicos para o tipo de pelagem e pele do animal, corte de unhas, limpeza das orelhas e tosa personalizada (higiênica ou estilizada).",
     duration: "1h",
     price: "$50",
     icon: <Scissors />,
-    linkText: 'Olá, vi no site sobre Banho e tosa e gostaria de mais informações.'
+    linkText:
+      "Olá, vi no site sobre Banho e tosa e gostaria de mais informações.",
   },
   {
     title: "Consulta Veterinária",
-    description: "Oferece atendimento clínico básico ou especializado para cuidar da saúde do animal. Inclui diagnóstico de doenças, aplicação de vacinas obrigatórias.",
+    description:
+      "Oferece atendimento clínico básico ou especializado para cuidar da saúde do animal. Inclui diagnóstico de doenças, aplicação de vacinas obrigatórias.",
     duration: "1h",
     price: "$45",
     icon: <Syringe />,
-    linkText: 'Olá, vi no site sobre Consulta veterinária e gostaria de mais informações.'
+    linkText:
+      "Olá, vi no site sobre Consulta veterinária e gostaria de mais informações.",
   },
   {
     title: "Táxi Pet",
-    description: "Serviço de transporte para levar e buscar os pets no petshop, clínicas veterinárias ou outros locais. Ideal para tutores que não têm tempo ou transporte adequado para locomover os animais.",
+    description:
+      "Serviço de transporte para levar e buscar os pets no petshop, clínicas veterinárias ou outros locais. Ideal para tutores que não têm tempo ou transporte adequado para locomover os animais.",
     duration: "2h",
     price: "$80",
     icon: <CarTaxiFront />,
-    linkText: 'Olá, vi no site sobre Táxi Pet e gostaria de mais informações.'
+    linkText: "Olá, vi no site sobre Táxi Pet e gostaria de mais informações.",
   },
   {
     title: "Hotel para pets",
-    description: "Serviço de hospedagem para animais de estimação, ideal para quando os tutores precisam viajar ou se ausentar por alguns dias. Os pets ficam acomodados em espaços seguros, confortáveis.",
+    description:
+      "Serviço de hospedagem para animais de estimação, ideal para quando os tutores precisam viajar ou se ausentar por alguns dias. Os pets ficam acomodados em espaços seguros, confortáveis.",
     duration: "1h",
     price: "$60",
     icon: <Hotel />,
-    linkText: 'Olá, vi no site sobre Hotel para pets e gostaria de mais informações.'
+    linkText:
+      "Olá, vi no site sobre Hotel para pets e gostaria de mais informações.",
   },
 ];
 
 export function Services() {
-
-  const [emblaRef, emblaApi] = useEmblaCarousel({ 
+  const [emblaRef, emblaApi] = useEmblaCarousel({
     loop: false,
-    align: 'start',
+    align: "start",
     slidesToScroll: 1,
     breakpoints: {
       "min-width: 768px": {
-        slidesToScroll: 3
+        slidesToScroll: 3,
       },
-    }
+    },
   });
 
   function scrollPrev() {
@@ -58,87 +72,83 @@ export function Services() {
   function scrollNext() {
     emblaApi?.scrollNext();
   }
-  
+
   return (
     <section className="bg-white py-16">
-      <div 
-        className="container mx-auto px-4" 
+      <div
+        className="container mx-auto px-4"
         data-aos="fade-left"
         data-aos-anchor="#example-anchor"
         data-aos-offset="500"
-        data-aos-duration="500">
-        
+        data-aos-duration="500"
+      >
         <h2 className="text-4xl font-bold mb-12">Serviços</h2>
-          
-        <div className="relative">
 
+        <div className="relative">
           <div className="overflow-hidden" ref={emblaRef}>
             <div className="flex">
               {services.map((item, index) => (
                 // O flex-[0_0_100%] quer dizer que o item vai ocupar 100% do espaço disponível
-                <div key={index} className="flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3">
+                <div
+                  key={index}
+                  className="flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3"
+                >
                   <article className="bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col">
                     {/* o flex-1 é para ele pegar a tela toda */}
-                    <div className="flex-1 flex items-start justify-between">  
-                      
+                    <div className="flex-1 flex items-start justify-between">
                       <div className="flex gap-3">
                         <span className="text-3xl">{item.icon}</span>
                         <div>
-                          <h3 className="font-bold text-xl my-1">{item.title}</h3>
+                          <h3 className="font-bold text-xl my-1">
+                            {item.title}
+                          </h3>
                           <p className="text-gray-400 text-sm select-none">
                             {item.description}
                           </p>
                         </div>
                       </div>
-
                     </div>
 
                     <div className="border-t border-gray-700 pt-4 flex items-center justify-between flex-col">
                       <div className="flex justify-between gap-2 text-sm w-full">
                         <div className="flex items-center gap-2">
-                          <Clock className="w-5 h-5"/>
+                          <Clock className="w-5 h-5" />
                           <span>{item.duration}</span>
                         </div>
                         <p>{item.price}</p>
                       </div>
                       <div className="flex items-center pt-3 w-full">
-                        <a 
+                        <a
                           target="_blank"
                           href={`https://wa.me/5599999999999/?text=Olá, vi no site sobre Banho e tosa e gostaria de agendar um horario. sobre ${item.title}`}
                           className="flex bg-yellow-400 text-black w-full items-center justify-center gap-2 hover:bg-yellow-500 px-4 py-2 rounded-md duration-300"
                         >
-                          <WhatsappLogo className="w-5 h-5"/>
+                          <WhatsappLogo className="w-5 h-5" />
                           Agendar horario
                         </a>
                       </div>
                     </div>
-
                   </article>
                 </div>
               ))}
             </div>
           </div>
-          
-          <button 
-            onClick={scrollPrev} 
+
+          <button
+            onClick={scrollPrev}
             className="bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10"
           >
-            <ChevronLeft 
-              className="w-6 h-6 text-gray-600"
-            />
+            <ChevronLeft className="w-6 h-6 text-gray-600" />
           </button>
 
-          <button 
-            onClick={scrollNext} 
+          <button
+            onClick={scrollNext}
             className="bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-6 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10"
           >
-            <ChevronRight 
-              className="w-6 h-6 text-gray-600"
-            />
+            <ChevronRight className="w-6 h-6 text-gray-600" />
           </button>
-
         </div>
       </div>
     </section>
-  )
+  );
 }
