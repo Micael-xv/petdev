fa8e48633166c3a04e4364a92a710f4e58333bb9
Micael <micaelxv1@gmail.com>
2025-04-21 20:59:43 -0300

    fix: ajustes

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,81 +1,78 @@
-import { WhatsappLogo } from '@phosphor-icons/react/dist/ssr'; // dist/ssr é para importar o ícone no servidor
-import  dogImg  from '../../../public/hero-dog.webp';
-import catImg from '../../../public/cat-hero.png';
-import Image from 'next/image';
+import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr"; // dist/ssr é para importar o ícone no servidor
+import dogImg from "../../../public/hero-dog.webp";
+import catImg from "../../../public/cat-hero.png";
+import Image from "next/image";
 
 export function Hero() {
   return (
     <section className="bg-[#E84C3D] text-white relative overflow-hidden">
-
       <div>
-        <Image 
+        <Image
           src={dogImg}
-          alt='Foto do cachorro'
+          alt="Foto de um cachorro"
           fill
-          sizes='100vw'
+          sizes="100vw"
           priority
-          className='object-cover opacity-60 lg:hidden'
+          className="object-cover opacity-60 lg:hidden"
         />
-        <div className='absolute inset-0 bg-black opacity-40 md:hidden'></div>
+        <div className="absolute inset-0 bg-black opacity-40 md:hidden"></div>
       </div>
 
-      <div className='container mx-auto pt-16 pb-16 md:pb-0 px-4 relative'>
-
-        <article className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
-
-          <div className='space-y-6'>
-            <h1 
-              className="text-3xl md:text-4xl lg:text-5xl font-bold leadign-10" 
+      <div className="container mx-auto pt-16 pb-16 md:pb-0 px-4 relative">
+        <article className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <div className="space-y-6">
+            <h1
+              className="text-3xl md:text-4xl lg:text-5xl font-bold leadign-10"
               data-aos="fade-down"
             >
               Seu pet merece cuidado, carinho e atenção especial.
             </h1>
             <p className="lg:text-lg" data-aos="fade-down">
-              Oferecemos os melhores serviços para garantir o bem-estar e a felicidade do seu amigo de quatro patas.
+              Oferecemos os melhores serviços para garantir o bem-estar e a
+              felicidade do seu amigo de quatro patas.
             </p>
 
-            <a 
+            <a
               href="#"
               data-aos="fade-right"
-              className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit gap-2"            >
-              <WhatsappLogo className='w-5 h-5'/>
+              className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit gap-2"
+            >
+              <WhatsappLogo className="w-5 h-5" />
               Contato via WhatsApp
             </a>
 
             <div className="mt-8" data-aos="fade-right">
               <p className="text-sm mb-4">
-                <b className="bg-black text-white px-2 py-1 rounded-md">5%</b> de desconto na primeira compra.
+                <b className="bg-black text-white px-2 py-1 rounded-md">5%</b>{" "}
+                de desconto na primeira compra.
               </p>
 
-              <div className='flex mt-4'>
-                <div className='w-32 hidden lg:block'>
+              <div className="flex mt-4">
+                <div className="w-32 hidden lg:block">
                   <Image
                     src={catImg}
-                    alt='Foto do gato'
+                    alt="Foto do gato"
                     quality={100}
-                    className='object-fill'
+                    className="object-fill"
                   />
                 </div>
               </div>
             </div>
-
           </div>
 
-          <div className='hidden md:block h-full relative'>
+          <div className="hidden md:block h-full relative">
             <Image
               src={dogImg}
-              alt='Foto do cachorro'
-              className='object-contain' // para a imagem encaixar no tamanho inteiro
+              alt="Foto do cachorro"
+              className="object-contain" // para a imagem encaixar no tamanho inteiro
               fill
-              sizes='(max-width: 768px) 0vw, 50vw'
+              sizes="(max-width: 768px) 0vw, 50vw"
               quality={100}
               priority
             />
           </div>
-
         </article>
       </div>
-
     </section>
-  )
+  );
 }
